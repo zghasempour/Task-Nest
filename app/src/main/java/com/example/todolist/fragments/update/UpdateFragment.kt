3296e5e7031718bb4609e1057ad1606108334914
Zahra Ghasempour Moghaddam <zahraghasempourmoghaddam@Zahra-MacBook-Air.local>
2024-08-23 16:05:43 +0300

    add navigation and pages

---
+++ app/src/main/java/com/example/todolist/fragments/update/UpdateFragment.kt
@@ -0,0 +1,20 @@
+package com.example.todolist.fragments.update
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todolist.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+}
