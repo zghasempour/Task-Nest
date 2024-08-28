06adf25569cab6c9a657ca053d1b28be383d6dcb
Zahra Ghasempour Moghaddam <zahraghasempourmoghaddam@Zahra-MacBook-Air.local>
2024-08-28 10:37:39 +0300

    add navController

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.navigation.safe.args)
-    implementation(libs.gradle.build.tools)
 }
--- app/src/main/java/com/example/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todolist/fragments/list/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todolist.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ListFragment : Fragment() {
@@ -15,7 +17,12 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        val floatingBT:FloatingActionButton = view.findViewById(R.id.floatingBT)
+        floatingBT.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        return view
     }
 
 
